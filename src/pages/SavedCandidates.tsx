fa9290feb48361b5adafaef6aba8e2a43d511361
Mina <TMDHammond@outlook.com>
2025-03-29 13:16:00 -0700

    Updated Nav and gitignore

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,5 +1,7 @@
+// import React from 'react';
 import { useEffect, useState } from 'react';
 import Candidate from '../interfaces/Candidate.interface';
+
 
 const SavedCandidates = () => {
   const [savedCandidates, setSavedCandidates] = useState<Candidate[]>([]);
